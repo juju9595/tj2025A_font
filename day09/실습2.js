8aec4b1f82e350f5c3b36f5a4d453319776dab68
juju9595 <eunju220926@gmail.com>
2025-06-09 16:44:46 +0900

    Day9 실습

---
+++ day09/실습2.js
@@ -0,0 +1,27 @@
+
+let 이름1=prompt( "(1) 이름" )
+console.log( 이름1 )
+
+let 메뉴=prompt( "(2) 메뉴" )
+console.log( 메뉴 )
+
+let 수량=prompt( "(3) 수량" )
+console.log( 수량 )
+
+
+
+
+let 이름2=prompt( "(1) 이름 ")
+console.log( 이름2 )
+
+let 운동종류=prompt( " (2) 운동종류 ")
+console.log( 운동종류 )
+
+let 운동시간=prompt( " (3) 운동시간 ")
+console.log( 운동시간 )
+
+let 칼로리=운동시간*7
+console.log(칼로리)
+
+
+
