54dc8044b0ecb92cd39a6eb7e9a91d8bb0679fb0
juju9595 <eunju220926@gmail.com>
2025-06-16 18:19:42 +0900

    0616

---
+++ 연습.js
@@ -0,0 +1,12 @@
+let point1 = Number(prompt('점수:1:'));
+let point2 = Number(prompt('점수2 :'));
+let sum = point1+point2
+let result1 = sum >= 90 ? ""
+if(sum>= 90){
+    console.log('성공')
+}else if{
+    console.log('실패')
+}
+
+let id = prompt("아이디:")
+let pwd = prompt("비밀번호:")
